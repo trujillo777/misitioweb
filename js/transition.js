02b55f38af3c3d79ffbbe9ca75a71db950a05fcb
trujillo777 <trujilloandrea965@gmail.com>
2022-12-15 11:20:28 -0500

    jhvbfjhvb

--- js/transition.js
+++ js/transition.js
@@ -10,3 +10,31 @@ box3.addEventListener("click", function() {
         box3.classList.add("box3-old");
     }
 })
+
+let box4 = document.getElementById("box4");
+let box5 = document.getElementById("box5");
+
+box4.addEventListener("click", function() {
+    if(box5.classList.contains("box5")) {
+        box5.classList.remove("box5");
+    }
+    else {
+        box5.classList.add("box5");
+    }
+})
+
+let box6= document.getElementById("box6");
+let box7=document.getElementById("box7");
+
+
+box6.addEventListener('click',function(){
+    if(box7.classList.contains('box7')){
+    box7.classList.remove('box7')
+    box6.classList.remove('box_6')
+}else{
+    box7.classList.add('box7')
+    box6.classList.add('box_6')
+}
+})
+
+
