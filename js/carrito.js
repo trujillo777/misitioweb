e0dc8d6e50c423e27cb248a5158e860111ce92f9
trujillo777 <trujilloandrea965@gmail.com>
2023-03-30 09:02:17 -0500

    hdyt

---
+++ js/carrito.js
@@ -0,0 +1,5 @@
+let imgBox = document.getElementById(imgBox);
+let imgMini1 = document.getElementById("imgMini1");
+let imgMini2 = document.getElementById("imgMini2");
+let imgMini3 = document.getElementById("imgMini3");
+let imgMini4 = document.getElementById("imgMini4");
