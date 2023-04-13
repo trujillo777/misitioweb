f0b096f9f9364a2ccffb7e0a6e8937694058e680
trujillo777 <trujilloandrea965@gmail.com>
2023-04-13 11:28:39 -0500

    sdhb

--- js/carrito.js
+++ js/carrito.js
@@ -3,27 +3,99 @@ let imgMini1 = document.getElementById("imgMini1");
 let imgMini2 = document.getElementById("imgMini2");
 let imgMini3 = document.getElementById("imgMini3");
 let imgMini4 = document.getElementById("imgMini4");
+let boxImgMini1 = document.getElementById("BoxImgMini1")
+let boxImgMini2 = document.getElementById("BoxImgMini2")
+let boxImgMini3 = document.getElementById("boxImgMini3")
+let boxImgMini4 = document.getElementById("boxImgMini4")
+
 
 imgMini1.addEventListener("click", function(){
     imgBox.setAttribute("src", "img/image-product-1  - copia.jpg")
-    boxImgMini1.classlist.add("box-img-mini")
-    boxImgMini2.classlist.remove("box-img-mini")
-    boxImgMini3.classlist.remove("box-img-mini")
-    boxImgMini4.classlist.remove("box-img-mini")
-    boxImgMini1.classlist.add("img-mini-act")
-    boxImgMini2.classlist.remove("img-mini-act")
+    boxImgMini1.classList.add("box-img-mini")
+    boxImgMini2.classList.remove("box-img-mini")
+    boxImgMini3.classList.remove("box-img-mini")
+    boxImgMini4.classList.remove("box-img-mini")
+    imgMini1.classList.add("img-mini-act")
+    imgMini2.classList.remove("img-mini-act")
+    imgMini3.classList.remove("img-mini-act")
+    imgMini4.classList.remove("img-mini-act")
 })
 
 imgMini2.addEventListener("click", function(){
     imgBox.setAttribute("src", "img/image-product-2  - copia.jpg")
+    boxImgMini2.classList.add("box-img-mini")
+    boxImgMini1.classList.remove("box-img-mini")
+    boxImgMini3.classList.remove("box-img-mini")
+    boxImgMini4.classList.remove("box-img-mini")
+    imgMini2.classList.add("img-mini-act")
+    imgMini1.classList.remove("img-mini-act")
+    imgMini3.classList.remove("img-mini-act")
+    imgMini4.classList.remove("img-mini-act")
 })
 
 imgMini3.addEventListener("click", function(){
     imgBox.setAttribute("src", "img/image-product-3  - copia.jpg")
+    boxImgMini1.classList.remove("box-img-mini")
+    boxImgMini2.classList.remove("box-img-mini")
+    boxImgMini3.classList.add("box-img-mini")
+    boxImgMini4.classList.remove("box-img-mini")
+    imgMini2.classList.remove("img-mini-act")
+    imgMini1.classList.remove("img-mini-act")
+    imgMini3.classList.add("img-mini-act")
+    imgMini4.classList.remove("img-mini-act")
+
 })
 
 imgMini4.addEventListener("click", function(){
     imgBox.setAttribute("src", "img/image-product-4  - copia.jpg")
+    boxImgMini1.classList.remove("box-img-mini")
+    boxImgMini2.classList.remove("box-img-mini")
+    boxImgMini3.classList.remove("box-img-mini")
+    boxImgMini4.classList.add("box-img-mini")
+    imgMini2.classList.remove("img-mini-act")
+    imgMini1.classList.remove("img-mini-act")
+    imgMini3.classList.remove("img-mini-act")
+    imgMini4.classList.add("img-mini-act")
 })
 
 
+
+
+
+let btnRestar = document.getElementById("btnRestar");
+let btnSumar = document.getElementById("btnSumar");
+let btnCantidad = document.getElementById("btnCantidad")
+let cantBox = document.getElementById("cantBox");
+let numeroElementos = document.getElementById("numeroElementos");
+
+
+
+btnSumar.addEventListener("click", function() {
+    
+   let numeroElementosTxt = document.getElementById("numeroElemetos").innerHTML;
+   
+   let numeroElementosINT = parseInt(numeroElementosTxt);
+
+   let totalElementosINT = numeroElementosInt + 1;
+
+   let totalElementostxt = totalElementosInt.toString();
+
+   numeroElementos.innerHTML = totalElementos;
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
