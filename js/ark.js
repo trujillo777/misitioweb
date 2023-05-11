d59e2f422abfeff8c730417e4ae37421c84cb2b3
trujillo777 <trujilloandrea965@gmail.com>
2023-05-11 11:35:06 -0500

    kgreivbe

--- js/ark.js
+++ js/ark.js
@@ -1,7 +1,20 @@
-let Izquierda = document.getElementById("Izquierda");
+let derecha = document.getElementById("derecha");
+let izquierda = document.getElementById("izquierda");
 
-let Ark = document.getElementById("Ark");
+let imgArkCamry = document.getElementById("imgArkCamry");
+let imgArkTundra = document.getElementById("imgArkTundra");
+let imgArkRav4 = document.getElementById("imgArkRav4");
 
-Izquierda.addEventListener("click", function(){
-    Izquierda.classList.remove("img-ark-camry-hidden");
-})
+derecha.addEventListener("click", function(){
+    if (imgArkCamry.classList.contains("img-ark-camry")){
+        imgArkCamry.classList.remove("img-ark-camry");
+        imgArkCamry.classList.add("img-ark-camry-hidden");
+        imgArkTundra.classList.add("img-ark-tundra");
+        imgArkTundra.classList.remove("img-ark-tundra-hidden-right");
+    }
+    else if (imgArkTundra.classList.contains("img-ark-tundra")){
+        imgArkTundra.classList.remove("img-ark-tundra");
+        imgArkTundra.classList.add("img-ark-tundra-hidden-left");
+        imgArkRav4.classList.add("img-ark-rav4");
+        imgArkRav4.classList.remove("img-ark-rav4-hidden-right");
+    }})
