6da064a5fd1736d359236742becd38cbcababa1d
SENA <trujilloandrea965@gmail.com>
2023-11-30 11:39:55 -0500

    mejorado

--- js/puntuacion.js
+++ js/puntuacion.js
@@ -1,26 +1,25 @@
 function generarGrafica() {
-    let dato1 = parseFloat(document.getElementById("dato1").value);
-    let dato2 = parseFloat(document.getElementById("dato2").value);
-    let dato3 = parseFloat(document.getElementById("dato3").value);
-    let dato4 = parseFloat(document.getElementById("dato4").value);
-    let dato5 = parseFloat(document.getElementById("dato5").value);
+    const datos = document.querySelectorAll('.input');
 
-    let total = dato1 + dato2 + dato3 + dato4 + dato5;
+    let total = 0;
+    for (let i = 0; i < datos.length; i++) {
 
-    let porcentajeDato1 = (dato1 / total) * 100;
-    let porcentajeDato2 = (dato2 / total) * 100;
-    let porcentajeDato3 = (dato3 / total) * 100;
-    let porcentajeDato4 = (dato4 / total) * 100;
-    let porcentajeDato5 = (dato5 / total) * 100;
+        if (isNaN(datos[i].value)) datos[i].value = 0;
+        total += Number(datos[i].value);
+    }
+
+    let porcentajes = [];
+    for (let i = 0; i < datos.length; i++) {
+        if (isNaN(datos[i].value)) datos[i].value = 0;
+        porcentajes[i] = (datos[i].value / total) * 100;
+    }
 
     let barContainer = document.getElementById("chartContainer");
-    barContainer.innerHTML = ""; 
-    
-    crearBarra("Dato 1", porcentajeDato1, { backgroundColor: "#FFCE56" });
-    crearBarra("Dato 2", porcentajeDato2, { backgroundColor: "#FFCE56" });
-    crearBarra("Dato 3", porcentajeDato3, { backgroundColor: "#FFCE56" });
-    crearBarra("Dato 4", porcentajeDato4, { backgroundColor: "#FFCE56" });
-    crearBarra("Dato 5", porcentajeDato5, { backgroundColor: "#FFCE56" });
+    barContainer.innerHTML = "";
+
+    for (let i = 0; i < porcentajes.length; i++) {
+        crearBarra("", porcentajes[i], { backgroundColor: "#FFCE56" });
+    }
 }
 
 function crearBarra(label, porcentaje, estilos) {
@@ -30,14 +29,15 @@ function crearBarra(label, porcentaje, estilos) {
     bar.style.display = "flex";
     bar.style.alignItems = "center";
     bar.style.marginBottom = "10px";
-    bar.style.border = "1px solid #ddd"; 
-    bar.style.maxWidth = "400px"; 
-    bar.style.transition = "background-color 0.3s"; 
+    bar.style.border = "1px solid #ddd";
+    bar.style.maxWidth = "400px";
 
     let innerBar = document.createElement("div");
     innerBar.style.height = "20px";
     innerBar.style.marginRight = "5px";
     innerBar.style.width = porcentaje + "%";
+    innerBar.style.transition = 'all 1s';
+    innerBar.innerText = Math.floor(porcentaje) + "%";
 
 
     for (let estilo in estilos) {
